11f29fa49e949c354402faa8a0a4c11c6e48afdc
Tanaykumar01 <tanayjhunjhunwala.official@gmail.com>
2025-08-15 21:13:51 +0530

    feat: enhance cookie security by adding SameSite attribute and fix error handling status code

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -73,7 +73,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly : true,
-        secure : true
+        secure : true,
+        sameSite: "None"
     }
 
     return res.status(200).cookie("refreshToken", refreshToken, options).cookie("accessToken", accessToken, options).json(
